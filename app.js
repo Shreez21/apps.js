be6d2c2f4ab739928785aa3df044da5e3ae010cd
Shreez21 <110349291+Shreez21@users.noreply.github.com>
2022-08-11 14:16:54 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const firstName = "Sri";
+let lastName = "Sivakumar";
+var age = 22;
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+let title = "Name of the Wind";
+let Author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
